6ffad9cf5f1279979ceb1c6e35708e36fb09581f
losina <jinalo@sas.upenn.edu>
2019-07-12 11:39:12 -0400

    last version before Gatsby

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(bodyParser.urlencoded({"extended": true}))
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.render("home")
+})
+
+app.get('/about', function(req, res) {
+    res.render("about")
+})
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
