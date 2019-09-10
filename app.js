44e474b8c711ca267fda11686610c84fe5c592fe
losina <jinalo@sas.upenn.edu>
2019-09-10 15:24:15 -0400

    mainpage theme set up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
 const app = express();
+
 app.use(bodyParser.urlencoded({"extended": true}))
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -11,8 +11,12 @@ app.get('/', function(req, res) {
     res.render("home")
 })
 
-app.get('/about', function(req, res) {
-    res.render("about")
+app.get('/design', function(req, res) {
+    res.render("design")
+})
+
+app.get('/programming', function(req, res) {
+    res.render("programming")
 })
 
 
