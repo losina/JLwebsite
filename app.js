971741a1d94cf3f9d8a0e70f702f8c3686f6bedd
losina <jinalo@sas.upenn.edu>
2019-09-11 22:42:12 -0400

    design page completed

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ app.use(express.static("public"));
 app.get('/', function(req, res) {
     res.render("home")
 })
+app.get('/home', function(req, res) {
+    res.redirect("/")
+})
 
 app.get('/design', function(req, res) {
     res.render("design")
 })
 
 app.get('/programming', function(req, res) {
-    res.render("programming")
+    r
+    es.render("programming")
 })
 
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
-  
